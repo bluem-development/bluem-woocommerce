d1fda0ee3e88fa7aaf83066d82200e0e97ed8e35
Daan Rijpkema <info@codex.management>
2020-03-15 14:38:19 +0100

    Initial commit

---
+++ bluem-woocommerce.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name: bluem-woocommerce
+ * Version: 1.0.0
+ * Plugin URI: http://www.hughlashbrooke.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Hugh Lashbrooke
+ * Author URI: http://www.hughlashbrooke.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: bluem-woocommerce
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Hugh Lashbrooke
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Load plugin class files.
+require_once 'includes/class-bluem-woocommerce.php';
+require_once 'includes/class-bluem-woocommerce-settings.php';
+
+// Load plugin libraries.
+require_once 'includes/lib/class-bluem-woocommerce-admin-api.php';
+require_once 'includes/lib/class-bluem-woocommerce-post-type.php';
+require_once 'includes/lib/class-bluem-woocommerce-taxonomy.php';
+
+/**
+ * Returns the main instance of bluem-woocommerce to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object bluem-woocommerce
+ */
+function bluem-woocommerce() {
+	$instance = bluem-woocommerce::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = bluem-woocommerce_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+bluem-woocommerce();
---
+++ includes/lib/class-bluem-woocommerce-taxonomy.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Taxonomy functions file.
+ *
+ * @package WordPress Plugin Template/Includes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Taxonomy functions class.
+ */
+class bluem-woocommerce_Taxonomy {
+
+	/**
+	 * The name for the taxonomy.
+	 *
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $taxonomy;
+
+	/**
+	 * The plural name for the taxonomy terms.
+	 *
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $plural;
+
+	/**
+	 * The singular name for the taxonomy terms.
+	 *
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $single;
+
+	/**
+	 * The array of post types to which this taxonomy applies.
+	 *
+	 * @var     array
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $post_types;
+
+	/**
+	 * The array of taxonomy arguments
+	 *
+	 * @var     array
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $taxonomy_args;
+
+	/**
+	 * Taxonomy constructor.
+	 *
+	 * @param string $taxonomy Taxonomy variable nnam.
+	 * @param string $plural Taxonomy plural name.
+	 * @param string $single Taxonomy singular name.
+	 * @param array  $post_types Affected post types.
+	 * @param array  $tax_args Taxonomy additional args.
+	 */
+	public function __construct( $taxonomy = '', $plural = '', $single = '', $post_types = array(), $tax_args = array() ) {
+
+		if ( ! $taxonomy || ! $plural || ! $single ) {
+			return;
+		}
+
+		// Post type name and labels.
+		$this->taxonomy = $taxonomy;
+		$this->plural   = $plural;
+		$this->single   = $single;
+		if ( ! is_array( $post_types ) ) {
+			$post_types = array( $post_types );
+		}
+		$this->post_types    = $post_types;
+		$this->taxonomy_args = $tax_args;
+
+		// Register taxonomy.
+		add_action( 'init', array( $this, 'register_taxonomy' ) );
+	}
+
+	/**
+	 * Register new taxonomy
+	 *
+	 * @return void
+	 */
+	public function register_taxonomy() {
+		//phpcs:disable
+		$labels = array(
+			'name'                       => $this->plural,
+			'singular_name'              => $this->single,
+			'menu_name'                  => $this->plural,
+			'all_items'                  => sprintf( __( 'All %s', 'bluem-woocommerce' ), $this->plural ),
+			'edit_item'                  => sprintf( __( 'Edit %s', 'bluem-woocommerce' ), $this->single ),
+			'view_item'                  => sprintf( __( 'View %s', 'bluem-woocommerce' ), $this->single ),
+			'update_item'                => sprintf( __( 'Update %s', 'bluem-woocommerce' ), $this->single ),
+			'add_new_item'               => sprintf( __( 'Add New %s', 'bluem-woocommerce' ), $this->single ),
+			'new_item_name'              => sprintf( __( 'New %s Name', 'bluem-woocommerce' ), $this->single ),
+			'parent_item'                => sprintf( __( 'Parent %s', 'bluem-woocommerce' ), $this->single ),
+			'parent_item_colon'          => sprintf( __( 'Parent %s:', 'bluem-woocommerce' ), $this->single ),
+			'search_items'               => sprintf( __( 'Search %s', 'bluem-woocommerce' ), $this->plural ),
+			'popular_items'              => sprintf( __( 'Popular %s', 'bluem-woocommerce' ), $this->plural ),
+			'separate_items_with_commas' => sprintf( __( 'Separate %s with commas', 'bluem-woocommerce' ), $this->plural ),
+			'add_or_remove_items'        => sprintf( __( 'Add or remove %s', 'bluem-woocommerce' ), $this->plural ),
+			'choose_from_most_used'      => sprintf( __( 'Choose from the most used %s', 'bluem-woocommerce' ), $this->plural ),
+			'not_found'                  => sprintf( __( 'No %s found', 'bluem-woocommerce' ), $this->plural ),
+		);
+		//phpcs:enable
+		$args = array(
+			'label'                 => $this->plural,
+			'labels'                => apply_filters( $this->taxonomy . '_labels', $labels ),
+			'hierarchical'          => true,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_nav_menus'     => true,
+			'show_tagcloud'         => true,
+			'meta_box_cb'           => null,
+			'show_admin_column'     => true,
+			'show_in_quick_edit'    => true,
+			'update_count_callback' => '',
+			'show_in_rest'          => true,
+			'rest_base'             => $this->taxonomy,
+			'rest_controller_class' => 'WP_REST_Terms_Controller',
+			'query_var'             => $this->taxonomy,
+			'rewrite'               => true,
+			'sort'                  => '',
+		);
+
+		$args = array_merge( $args, $this->taxonomy_args );
+
+		register_taxonomy( $this->taxonomy, $this->post_types, apply_filters( $this->taxonomy . '_register_args', $args, $this->taxonomy, $this->post_types ) );
+	}
+
+}
