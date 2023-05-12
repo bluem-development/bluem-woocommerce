7e2c1b078f6d2d653a1947a0cde8b6dbf33738a8
Peter Meester <p.meester@meesterdevelopment.nl>
2023-05-12 03:50:21 +0200

    Added technical details

--- views/home.php
+++ views/home.php
@@ -38,5 +38,7 @@
 
     <p><strong>Plugin versie</strong><br />Versie <?php $bluem = get_plugin_data( WP_PLUGIN_DIR . '/bluem/bluem.php' ); echo $bluem['Version']; ?> (<a href="<?php echo admin_url('plugin-install.php?tab=plugin-information&plugin=bluem&TB_iframe=true&width=600&height=550'); ?>" target="_blank">Details bekijken</a>)</p>
 
+    <p><strong>Technische gegevens</strong><br />WooCommerce versie: <?php echo class_exists('WooCommerce') ? WC()->version : __('WooCommerce niet geinstalleerd.', 'bluem-woocommerce'); ?><br />PHP versie: <?php echo phpversion(); ?></p>
+
     <?php bluem_render_footer(); ?>
 </div>
