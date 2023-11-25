f0551189d4f78881cb0ebf0945dea749d31bf24e
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-11-25 17:54:28 +0100

    Acceptance tests

---
+++ tests/Acceptance/FirstCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Home');
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
